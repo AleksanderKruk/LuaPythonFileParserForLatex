b396107f026cd6ccc0f6e3ba1e7cbd6dcf2210e2
Corvus <olekolek@spoko.pl>
2023-04-13 09:12:46 +0200

    Added scanDefiningLine function

--- find_syntax.lua
+++ find_syntax.lua
@@ -1,7 +1,15 @@
 
 local function isPythonFunctionDefLine(line)
-    local first_indent, function_name = string.match(line, "(%s*)def%s*([%w_]+)%(.*")
-    return first_indent, function_name
+    local indent_level, function_name = string.match(line, "(%s*)def%s*([%w_]+)%(.*")
+    return indent_level, function_name
+end
+
+
+local function scanDefiningLine(line, structures_dictionary)
+    local indent_level, function_name = isPythonFunctionDefLine(line)
+    structures_dictionary["functions"][function_name] = { indent_level } 
+    struc
+    print("   ")
 end
 
 
@@ -9,10 +17,11 @@ function ParsePythonFile(file_name)
     local python_file = io.open(file_name)
     local found_functions = {}
     local found_classes = {}
+    local structures = {}
     local line_counter = 1
     if python_file then
         for line in python_file:lines() do
-            local first_indent, function_name = isPythonFunctionDefLine(line)
+            scanDefiningLine(line)
             -- print(first_indent)
             -- print(function_name)
             if first_indent or function_name then
