533e64e72fe8b2bb778a93111e2fbda0db344e61
Corvus <olekolek@spoko.pl>
2023-04-13 08:38:05 +0200

    managed to open file

--- find_syntax.lua
+++ find_syntax.lua
@@ -1,4 +1,17 @@
 
-local test = "Hello World"
 
-print(test)
+
+
+local python_file = io.open("test.py")
+
+if python_file then
+    for python_file in python_file:lines() do
+        print(python_file)
+
+    end
+    python_file:close()
+end
+
+
+
+
