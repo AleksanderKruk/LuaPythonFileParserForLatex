4b866b0b22238c40594b32a898b4a4c12f9ba50a
Corvus <olekolek@spoko.pl>
2023-04-13 08:48:57 +0200

    added first match

--- find_syntax.lua
+++ find_syntax.lua
@@ -1,18 +1,21 @@
 
-function Parse_python_file(file_name)
+function ParsePythonFile(file_name)
     local python_file = io.open(file_name)
-
+    local found_functions = {}
+    local found_classes = {}
     if python_file then
-        for python_file in python_file:lines() do
-            print(python_file)
+        for line in python_file:lines() do
+            local groups = string.match( line, "%s*def.*" )
+            if groups then
+                print(groups)
+            end
 
         end
         python_file:close()
     end
-
 end
 
 
-Parse_python_file("test.py")
+ParsePythonFile("test.py")
 
 
