c8d562ad5dfa0be963d369d8620c4a48f92d9d64
Corvus <olekolek@spoko.pl>
2023-04-13 10:47:54 +0200

    added more quasi boolean functions

--- find_syntax.lua
+++ find_syntax.lua
@@ -1,7 +1,23 @@
 
+local function printDict(dictionary)
+    for key, value in pairs(dictionary) do
+        print(key .. " : " .. value)
+    end
+end
+
 local function isPythonFunctionDefLine(line)
     local indent_level, function_name = string.match(line, "(%s*)def%s*([%w_]+)%(.*")
     return indent_level, function_name
+end
+
+
+local function isPythonDefLine(line)
+    return isPythonFunctionDefLine(line)
+end
+
+
+local function isPythonContinutationLine(line)
+    return not isPythonDefLine(line)
 end
 
 local function makeFunction(line_number, function_name, indent_level)
@@ -21,21 +37,14 @@ end
 
 
 local function makeEmptyStructureDictionary()
-    return makeStructureDictionary(nil, nil)
+    return makeStructureDictionary({}, {})
 end
 
 local function scanDefiningLine(line, line_number, structures_dictionary)
     local indent_level, function_name = isPythonFunctionDefLine(line)
-    print(function_name)
-    if function_name then
-        local tmp = makeFunction(line_number, function_name, indent_level)
-        print(tmp)
-        -- structures_dictionary.functions[function_name] = makeFunction(line_number, function_name, indent_level)
-        structures_dictionary.functions[function_name] = makeFunction(line_number, function_name, indent_level)
-    else
+    structures_dictionary.functions[function_name] = makeFunction(line_number, function_name, indent_level)
 
-    end
-
+    return function_name
     -- structures_dictionary.classes.function_name = { indent_level  }
 end
 
@@ -46,28 +55,34 @@ end
 
 function ParsePythonFile(file_name)
     local python_file = io.open(file_name)
-    local found_functions = {}
-    local found_classes = {}
     local structures = makeEmptyStructureDictionary()
-    local line_counter = 1
     if python_file then
-        for line in python_file:lines() do
-            scanDefiningLine(line, line_counter, structures)
+        local line_counter = 1
+        local all_lines = python_file:lines()
+        while line_counter ~= #all_lines do
+            local def_line = all_lines[line_counter]
+            local structure_name = scanDefiningLine(def_line, line_counter, structures)
+            if structure_name then
+                line_counter = line_counter + 1
+                local current_line = all_lines[line_counter]
+                while line_counter ~= #all_lines and isPythonContinutationLine(current_line) do
+                    
+                    line_counter = line_counter + 1
+                end
+            end
             line_counter = line_counter + 1
         end
         python_file:close()
-        print(structures)
+        -- print(structures)
     end
 end
 
 
 -- ParsePythonFile("test.py")
--- local testowa = {}
+-- local testowa = { 1, 2, 3, 4}
 -- testowa.a = "a"
 -- testowa.b = nil
 -- testowa["c"] = nil
 
--- print(
---     testowa.a
--- )
+-- print(testowa[1])
 
