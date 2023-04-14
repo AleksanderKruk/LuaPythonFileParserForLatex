5080881440a0e663757579d92d6d739fe1c1e5b8
Corvus <olekolek@spoko.pl>
2023-04-14 07:35:42 +0200

    preparation for general class prototype

---
+++ ClassPrototype.lua
@@ -0,0 +1,21 @@
+
+
+
+
+
+-- function PythonLanguageStructures:new(value)
+--   local obj = {}
+--   setmetatable(obj, self)
+--   obj.value = value
+--   return obj
+-- end
+
+
+-- function PythonLanguageStructures:setValue(new_value)
+--   self.value = new_value
+-- end
+
+
+-- function PythonLanguageStructures:getValue()
+--   return self.value
+-- end
