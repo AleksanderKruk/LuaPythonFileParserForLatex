567c46e8e039d8e03ea409a01622c7208eec2f82
Corvus <olekolek@spoko.pl>
2023-04-15 09:42:22 +0200

    refactored with new bracketify functions

--- latexintegrationfunctions.lua
+++ latexintegrationfunctions.lua
@@ -2,11 +2,12 @@ local myutils = require "myutils"
 local latexintegrationfunctions = {}
 
 function latexintegrationfunctions.toEnvironment(text, environment_name, optional_args)
-  local latex_text = "\\begin{" .. environment_name .. "}"
+  local braced_environment_name = myutils.addCurlyBrackets(environment_name)
+  local latex_text = "\\begin" .. braced_environment_name
   if optional_args then
-    latex_text = latex_text .. "[" .. optional_args .. "]"
+    latex_text = latex_text .. myutils.addSquareBrackets(optional_args)
   end
-  latex_text = latex_text .. "\n" .. text .. "\n\\end{" .. environment_name .. "}"
+  latex_text = latex_text .. "\n" .. text .. "\n\\end" .. braced_environment_name
   -- if optional_args then
   --   latex_text = latex_text .. "[" .. optional_args .. "]"
   -- end
