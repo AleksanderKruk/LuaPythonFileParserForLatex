345cb494c388cd7dfd56db554f3241dcb3ee74b6
Corvus <olekolek@spoko.pl>
2023-04-25 01:15:16 +0200

    added global testing tex variable

--- find_syntax.lua
+++ find_syntax.lua
@@ -7,7 +7,10 @@ local found_structures = parser:parseFile("test.py")
 -- local text = parser:getFunctionText("no_argument")
 local text = parser:getFunctionText("__main__")
 
-print(parser:makeListing("__main__"))
+tex = {}
+tex.print = print
+tex.print("aa")
+print(myutils.printListing(parser, "__main__"))
 -- print(text)
 -- print(text)
 -- myutils.printDictOfDicts(found_structures.functions)
