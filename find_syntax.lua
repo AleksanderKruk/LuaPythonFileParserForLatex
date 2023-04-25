860f35014d88db8481359657d5115335c940424c
Corvus <olekolek@spoko.pl>
2023-04-25 11:07:10 +0200

    test

--- find_syntax.lua
+++ find_syntax.lua
@@ -2,14 +2,13 @@ local myutils = require("myutils")
 local PythonFileParser = require("PythonFileParser")
 
 local parser = PythonFileParser:new()
-local found_structures = parser:parseFile("test.py")
+local found_structures = parser:parseFile("server.py")
 
 -- local text = parser:getFunctionText("no_argument")
 local text = parser:getFunctionText("__main__")
 
 tex = {}
 tex.print = print
-tex.print("aa")
 -- print(myutils.printListing(parser, "__main__"))
 print(myutils.printListingWithCaption(parser, "__main__", "GARNUCH"))
 -- print(text)
